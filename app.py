2f36179cbf512e6b1ea6d2879ecac9d07094706d
Elias Haroun <elias.haroun@gmail.com>
2024-10-27 09:39:52 -0700

    Change server message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get('/')
 def home():
-    return {"message": "Hello World"}
+    return {"message": "Hello World. From IK class."}
 
 if __name__ == "__main__":
     import uvicorn
